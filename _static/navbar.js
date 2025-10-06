daca7592a650f9e5d883917b9ab6c67d1b3c2333
hag007 <hagai.levi.007@gmail.com>
2025-10-06 16:00:12 +0300

    sync

--- _static/navbar.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toolbar = document.querySelector(".bd-header__toolbar");
-  const nav = document.querySelector(".navbar-end, .navbar-right, .bd-header__inner");
-  if (toolbar && nav) nav.appendChild(toolbar);
-});
