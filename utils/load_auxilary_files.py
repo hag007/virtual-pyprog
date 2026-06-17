67c6bc0a05e06128af69122dc8a31161a491ec64
mayamz <mayametz@gmail.com>
2026-06-17 14:05:15 +0300

    2526a->2526b

--- utils/load_auxilary_files.py
+++ utils/load_auxilary_files.py
@@ -15,7 +15,7 @@ except:
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-with urllib.request.urlopen('https://courses.cs.tau.ac.il/pyProg/2526a/resources/notebook_resources.zip') as f:
+with urllib.request.urlopen('https://courses.cs.tau.ac.il/pyProg/2526b/resources/notebook_resources.zip') as f:
     content = f.read()
 
 with open('files/notebook_resources.zip', 'wb') as f:
